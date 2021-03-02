62085f01f5fa0fa26c7b0fb2e4c0f1891ab3e7ca
LiamJly <joly.liam@gmail.com>
2021-03-02 11:58:41 +0100

    CRUD fonctionnels

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const PORT = 85
+app.use(express.json());
+
+
+var corsOptions = {
+    //origin: "http://localhost:4200"
+    origin: "*"
+  };
+
+app.use(cors(corsOptions));
+
+
+app.listen(
+    PORT,
+    ()=>{console.log(`Serveur Express a l ecoute sur le port ${PORT} `);}
+);
+
+// connexion de notre serveur à la base mongo
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'Gestibank';
+
+let db;
+
+MongoClient.connect(url, function(err, client) {
+     console.log("Connexion réussi avec Mongo");
+     db = client.db(dbName);
+    });
+
+
+app.get('/users', (req,res) => {
+      db.collection('user').find({}).toArray(function(err, docs) {
+          if (err) {
+                  console.log(err)
+                  throw err
+              }
+              res.status(200).json(docs)
+            }) 
+        });
+
+        app.get('/users/:id', async (req,res) => {
+              const login = req.params.id;
+              try {
+                  const docs = await db.collection('user').findOne({login})
+                  res.status(200).json(docs)
+              } catch (err) {
+                  console.log(err)
+                  throw err
+              }
+            });
+
+            app.post('/users', async (req,res) => {
+                  try {
+                      const userData = req.body
+                      const user  = await db.collection('user').insertOne(userData)
+                      res.status(200).json(user)
+                  } catch (err) {
+                      console.log(err)
+                      throw err
+                  }
+                });    
+                app.put('/users/:id', async (req,res) => {
+                      try {
+                          const login = req.params.id;
+                          const replacementUser = req.body;
+                          const user = await db.collection('user').replaceOne({login},replacementUser)
+                          res.status(200).json(user);
+                      } catch (err) {
+                          console.log(err)
+                          throw err
+                      }
+                    })
+
+                    app.delete('/users/:id', async (req,res) => {
+                          try {
+                              const login = req.params.id
+                              const user = await db.collection('user').deleteOne({login})
+                              res.status(200).json(user)
+                          } catch (err) {
+                              console.log(err)
+                              throw err
+                          } 
+                        })
